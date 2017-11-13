61e0d58d7816b4dc515adda0eb36332eeabd3621
Tony Boswell <tboswell@kc.rr.com>
2017-11-13 23:15:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,7 +20,7 @@ function forLoop(array) {
 function whileLoop(n) {
   while( n > 0 ) {
     console.log(--countdown);
-  } else {
-    console.log(`done.`);
- }
+  } 
+      console.log(`done.`);
+
 }
