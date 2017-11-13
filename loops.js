c8095fee761aa68c689b838ec26b8622d32ad01a
Tony Boswell <tboswell@kc.rr.com>
2017-11-13 23:56:39 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -25,9 +25,8 @@ function whileLoop(n) {
     return myString;
 }
 
-function maybeTrue() {
-  return Math.random() >= 0.5
-}
+// creates a function that emoves elements from `array` until `array` is empty or until
+// `maybeTrue()` returns `false`:
 
 function doWhileLoop(array) {
   function maybeTrue() {
